bea47aa21e9bc17e37e42bae58254b3b140384e9
Fikria12 <143469287+Fikria12@users.noreply.github.com>
2023-10-11 23:07:21 +0700

    Create Group 3 - SQL Assignment.sql

---
+++ Group 3 - SQL Assignment.sql
@@ -0,0 +1,7 @@
+## Case Study Questions
+
+---
+
+The following questions include some exploratory analysis of data from the founders. You and team are required to understand each scheme carefully before answer it.
+
+### Question 1: **Digital Analysis**
